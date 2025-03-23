c30b8882dff437f9735c62a222eefd29aa5cc740
Fagner Dev <fagnersro.dev@gmail.com>
2025-03-22 23:33:27 -0300

    creating rest equipment

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { EquipmentModule } from './equipment/equipment.module';
 
 @Module({
-  imports: [],
+  imports: [EquipmentModule],
   controllers: [],
   providers: [],
 })
