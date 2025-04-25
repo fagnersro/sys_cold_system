b33d62e8552b16471a349503f27de4c9b30bcd68
Fagner Dev <fagnersro.dev@gmail.com>
2025-04-24 23:14:12 -0300

    configuring store.dto and store.entity

--- back_end/src/stores/dto/create-store.dto.ts
+++ back_end/src/stores/dto/create-store.dto.ts
@@ -1,1 +1,45 @@
-export class CreateStoreDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  Matches,
+  ValidateNested,
+  IsNumber,
+} from '@nestjs/class-validator';
+import { Type } from 'class-transformer';
+
+class CoordinatesDto {
+  @IsNumber()
+  lat: number;
+
+  @IsNumber()
+  lng: number;
+}
+
+export class CreateStoreDto {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @Matches(/^\d{3}-\d{3}-\d{4}$/, {
+    message: 'Phone number must be in the format XXX-XXX-XXXX',
+  })
+  phone: string;
+
+  @IsString()
+  @IsOptional()
+  manager?: string;
+
+  @ValidateNested()
+  @Type((): new () => CoordinatesDto => CoordinatesDto)
+  coordinates: CoordinatesDto;
+}
--- back_end/src/stores/entities/store.entity.ts
+++ back_end/src/stores/entities/store.entity.ts
@@ -1,1 +1,55 @@
-export class Store {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+  ValidateNested,
+} from '@nestjs/class-validator';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import { Type } from 'class-transformer';
+
+class Coordinates {
+  @Column('float')
+  @IsNumber()
+  lat: number;
+
+  @Column('float')
+  @IsNumber()
+  lng: number;
+}
+
+@Entity('stores')
+export class Store extends BaseEntity {
+  @PrimaryColumn()
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @Column()
+  @IsString()
+  @Matches(/^\d{3}-\d{3}-\d{4}$/, {
+    message: 'Phone number must be in the format xxx-xxx-xxxx',
+  })
+  phone: string;
+
+  @Column({ nullable: true })
+  @IsString()
+  @IsOptional()
+  manager?: string;
+
+  @Column(() => Coordinates)
+  @ValidateNested()
+  @Type((): new () => Coordinates => Coordinates)
+  coordinates: Coordinates;
+}
